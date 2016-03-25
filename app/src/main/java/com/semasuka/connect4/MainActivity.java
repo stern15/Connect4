8bde2ba2340d8ea52c2b1a755cf76134d1401ebb
stern15 <ssemasuka@gmail.com>
2016-03-25 17:36:21 +0300

    now can set the counter from above the layout and set it by rotating from above

--- app/src/main/java/com/semasuka/connect4/MainActivity.java
+++ app/src/main/java/com/semasuka/connect4/MainActivity.java
@@ -3,12 +3,17 @@ package com.semasuka.connect4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     public void dropIn(View view){
 
+        ImageView counter=(ImageView) view;
 
+        counter.setTranslationY(-1000f);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationY(1000f).rotation(3600).setDuration(300);
 
     }
 
