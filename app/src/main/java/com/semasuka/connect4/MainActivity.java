a5d1d6f1fb75e338b0108cd862f5ed5fe566c4b1
stern15 <ssemasuka@gmail.com>
2016-03-25 23:58:32 +0300

    added algorithm to restrict a box to be repeated

--- app/src/main/java/com/semasuka/connect4/MainActivity.java
+++ app/src/main/java/com/semasuka/connect4/MainActivity.java
@@ -1,38 +1,48 @@
 package com.semasuka.connect4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     //0 for yellow and 1 for red
     int ActiveUser=0;
-    //2 means it is an unplayed slot within the grid
+    //2 means it is an unplayed slot within the grid(2 is placeholder temporary)
     int[] gameStatus={2,2,2,2,2,2,2,2,2};
 
-    public void dropIn(View view){
+    public void dropIn(View view) {
+
+
         //here we use view only use view because the view containt an empty image so there is no id
-        ImageView counter=(ImageView) view;
-        //set the translation above the layout
-        counter.setTranslationY(-1000f);
-        if (ActiveUser==0){
+        ImageView counter = (ImageView) view;
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
 
-        //we give the translation a yellow counter
-        counter.setImageResource(R.drawable.yellow);
-        ActiveUser=1;
+        if (gameStatus[tappedCounter] == 2) {
+
+            gameStatus[tappedCounter]=ActiveUser;
+
+
+            //set the translation above the layout
+            counter.setTranslationY(-1000f);
+            if (ActiveUser == 0) {
+
+
+                //we give the translation a yellow counter
+                counter.setImageResource(R.drawable.yellow);
+                ActiveUser = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+
+                ActiveUser = 0;
+            }
+            //now we animate it by the translation
+            counter.animate().translationYBy(1000f).rotationBy(360).setDuration(500);
+
         }
-
-        else{
-            counter.setImageResource(R.drawable.red);
-            ActiveUser=0;
-        }
-        //now we animate it by the translation
-        counter.animate().translationYBy(1000f).rotationBy(360).setDuration(500);
-
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
