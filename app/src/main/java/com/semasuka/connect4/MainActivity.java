fadcba2b7757ecd9a96ec85f7a5a997589a788c6
stern15 <ssemasuka@gmail.com>
2016-03-25 17:02:21 +0300

    adding the dropIn method

--- app/src/main/java/com/semasuka/connect4/MainActivity.java
+++ app/src/main/java/com/semasuka/connect4/MainActivity.java
@@ -2,8 +2,15 @@ package com.semasuka.connect4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
