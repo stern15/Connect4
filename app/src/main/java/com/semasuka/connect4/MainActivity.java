067c63476c7b1ad3eb0b6615354974bba0b1f5f8
stern15 <ssemasuka@gmail.com>
2016-03-26 12:11:38 +0300

    adding a winning game combinaison

--- app/src/main/java/com/semasuka/connect4/MainActivity.java
+++ app/src/main/java/com/semasuka/connect4/MainActivity.java
@@ -2,6 +2,7 @@ package com.semasuka.connect4;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     //2 means it is an unplayed slot within the grid(2 is placeholder temporary)
     int[] gameStatus={2,2,2,2,2,2,2,2,2};
 
-    int[][] winningGame={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{3,4,6}};
+    int[][] winningPositions={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     public void dropIn(View view) {
 
@@ -42,6 +43,23 @@ public class MainActivity extends AppCompatActivity {
             }
             //now we animate it by the translation
             counter.animate().translationYBy(1000f).rotationBy(360).setDuration(500);
+
+            //we created an integer array that will loop trought the winningPositions and will compare each index of winningPosition to the next index
+            //and check if the combinaison of all those index gives and at the end we are checking if its not 2 coz 2 mean that it is not yet played
+            for(int[]winningPosition:winningPositions){
+
+                if(gameStatus[winningPosition[0]]==gameStatus[winningPosition[1]] &&
+                        gameStatus[winningPosition[1]]==gameStatus[winningPosition[2]] &&
+                            gameStatus[winningPosition[0]]!=2){
+
+
+                    Log.i("gameStatus", String.valueOf(gameStatus[winningPosition[0]]));
+                }
+
+
+
+            }
+
 
         }
     }
